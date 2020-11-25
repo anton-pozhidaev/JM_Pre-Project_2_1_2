781ab7f30187af7c94854dbcc0ab1561ec0cb010
anton-pozhidaev <anton.pozhidaev@gmail.com>
2020-11-25 16:15:54 +0300

    task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hi World");
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+
+        System.out.println("Does beans HelloWorld equals? " + (bean == bean2));
+        System.out.println("Does beans Cat equals? " + (cat1 == cat2)
+                + "\n cat1 ref: " + cat1.toString()
+                + "\n cat2 ref: " + cat2.toString());
     }
 }
